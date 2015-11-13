4dfd71af2228f8916e092f0e86f911d5a90824bf
Cheikh Seck <cseck@bryant.edu>
2015-11-12 20:31:03 -0500

    Update ApiDemo.java

--- tests/tests/ApiDemo.java
+++ tests/tests/ApiDemo.java
@@ -14,9 +14,9 @@ public class ApiDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Console.log("Attempting Request ---");
-		MaxCDN api = new MaxCDN("jvmstix" , //alias
-				"0191654f5f1bec3dab351e5bfaee8c9605643fe53" , //Cons. key
-				"fd68f9acb93f82fe7f0862890770068a");	// Cons. Secret
+		MaxCDN api = new MaxCDN("" , //alias
+				"" , //Cons. key
+				"");	// Cons. Secret
 		
 		    //Console.log(api.getAuthUrl(api.getRequestToken()));
 			
